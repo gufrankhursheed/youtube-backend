cc02d4eb07737cc789527ad627079c37cff20884
unknown <gufrankhursheed7@gmail.com>
2024-12-24 13:36:10 +0530

    add subscription controllers

--- src/app.js
+++ src/app.js
@@ -14,10 +14,16 @@ app.use(cookieParser());
 //routes import
 
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/video", videoRouter);
 
 export default app

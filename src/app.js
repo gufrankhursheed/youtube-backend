020b0322dc11764231310278d39fdb1ba985ccd4
unknown <gufrankhursheed7@gmail.com>
2024-12-24 14:26:11 +0530

    add dashboard controllers

--- src/app.js
+++ src/app.js
@@ -17,13 +17,17 @@ import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export default app
